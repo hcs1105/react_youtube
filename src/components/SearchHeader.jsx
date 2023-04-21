7e6017a0d401a5cc99ab4f51a844e4ef1a364037
hcs1105 <hcs1105@gmail.com>
2023-04-21 21:03:37 +0900

    검색 헤더에 스타일 적용

--- src/components/SearchHeader.jsx
+++ src/components/SearchHeader.jsx
@@ -16,16 +16,16 @@ export default function SearchHeader() {
   }, [keyword]);
 
   return (
-    <header>
-      <h1>
-        <Link to='/'>
-          <AiFillYoutube />
+    <header className="w-full flex p-4 text-2xl border-b border-zinc-600 mb-4">
+      <h1 className="flex items-center font-bold mr-2">
+        <Link className="flex items-center" to='/'>
+          <AiFillYoutube className="text-brand mr-1" />
           <span>Youtube</span>
         </Link>
       </h1>
-      <form onSubmit={handleSubmit}>
-        <input type="text" placeholder="Search" value={text} onChange={(e) => setText(e.target.value)} />
-        <button type="submit"><AiOutlineSearch /></button>
+      <form className="w-full flex justify-center" onSubmit={handleSubmit}>
+        <input type="text" className="w-7/12 p-2 bg-black text-gray-50" placeholder="Search" value={text} onChange={(e) => setText(e.target.value)} />
+        <button type="submit" className="bg-zinc-600 px-4"><AiOutlineSearch /></button>
       </form>
     </header>
   );
