0e0fb25711a1f1737e7d809f91488074fe0901c7
hcs1105 <hcs1105@gmail.com>
2023-04-21 21:50:27 +0900

    Mock 데이터를 이용하여 비디오 목록 만들기

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import { Outlet } from "react-router-dom";
 import SearchHeader from "./components/SearchHeader";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 function App() {
+  const queryClient = new QueryClient();
+
   return (
     <>
       <SearchHeader />
-      <Outlet />
+      <QueryClientProvider client={queryClient}>
+        <Outlet />
+      </QueryClientProvider>
     </>
   );
 }
---
+++ src/components/VideoCard.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function VideoCard({ video }) {
+  return (
+    <li>
+      {video.snippet.title}
+    </li>
+  );
+}
