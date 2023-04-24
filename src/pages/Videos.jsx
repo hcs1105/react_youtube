b5df4ae6b987e96759dfeea94703e1c9f4ec5118
hcs1105 <hcs1105@gmail.com>
2023-04-24 17:20:56 +0900

    클래스를 이용하여 실제 데이터로 구성된 비디오 목록 만들기

--- src/pages/Videos.jsx
+++ src/pages/Videos.jsx
@@ -2,12 +2,12 @@ import { useQuery } from '@tanstack/react-query';
 import React from 'react';
 import { useParams } from 'react-router-dom';
 import VideoCard from '../components/VideoCard';
-import FakeYoutube from '../components/API/fakeYoube';
+import Youtube from '../components/API/youtube';
 
 export default function Videos() {
   const { keyword } = useParams();
   const { isLoading, error, data: videos } = useQuery(['videos', keyword], () => {
-    const youtube = new FakeYoutube();
+    const youtube = new Youtube();
 
     return youtube.search(keyword);
   });
