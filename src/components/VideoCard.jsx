839c7e7a568b16031d15cc09a6cb376e5088b5ad
hcs1105 <hcs1105@gmail.com>
2023-04-24 21:48:19 +0900

    Youtube API에서 비디오 개별 항목에 필요한 데이터 연동

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import { formatAgo } from '../util/date';
 
 export default function VideoCard({ video }) {
+  const {title, thumbnails, channelTitle, publishedAt} = video.snippet;
+
   return (
     <li>
-      {video.snippet.title}
+      <img src={thumbnails.medium.url} alt={title} />
+      <div>
+        <h2>{title}</h2>
+        <p>{channelTitle}</p>
+        <p>{formatAgo(publishedAt, 'ko')}</p>
+      </div>
     </li>
   );
 }
