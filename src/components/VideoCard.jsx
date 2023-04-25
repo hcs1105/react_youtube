c11682c9d103ab2e9fbf227d3daa125cc61b1104
hcs1105 <hcs1105@gmail.com>
2023-04-25 18:31:03 +0900

    비디오 상세 페이지 마크업 작성 중

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { formatAgo } from '../util/date';
+import { useNavigate } from 'react-router-dom';
 
 export default function VideoCard({ video }) {
   const {title, thumbnails, channelTitle, publishedAt} = video.snippet;
+  const navigate = useNavigate();
 
   return (
-    <li>
+    <li onClick={() => {navigate(`/videos/watch/${video.id}`, {state: { video }})}}>
       <img src={thumbnails.medium.url} className="w-full" alt="" />    
       <h2 className="font-bold my-2 line-clamp-2">{title}</h2>
       <p className="text-sm opacity-75">{channelTitle}</p>
--- src/pages/VideoDetail.jsx
+++ src/pages/VideoDetail.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import { useLocation } from 'react-router-dom';
 
 export default function VideoDetail() {
+  const { state: { video } } = useLocation();
+
   return (
-    <div>Video Detail</div>
+    // eslint-disable-next-line 
+    <iframe 
+      id="player" 
+      type="text/html" 
+      width="100%" 
+      height="640" 
+      src={`http://www.youtube.com/embed/${video.id}`} 
+      frameborder="0" 
+      title="" />
   );
 }
