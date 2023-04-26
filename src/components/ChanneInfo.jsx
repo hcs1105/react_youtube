0c3ebec087fbecb29c0170ee435d709adc0cd252
hcs1105 <hcs1105@gmail.com>
2023-04-26 18:29:55 +0900

    비디오 상세 페이지 마크업 작성 완료

---
+++ src/components/ChanneInfo.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function ChanneInfo({ name }) {
+  return (
+    <div>
+      {name}
+    </div>
+  );
+}
--- src/pages/VideoDetail.jsx
+++ src/pages/VideoDetail.jsx
@@ -1,18 +1,33 @@
 import React from 'react';
 import { useLocation } from 'react-router-dom';
+import ChanneInfo from '../components/ChanneInfo';
+import RelatedVideos from '../components/RelatedVideos';
 
 export default function VideoDetail() {
   const { state: { video } } = useLocation();
-
+  const { title, channelId, channelTitle, description } = video.snippet;
+ 
   return (
-    // eslint-disable-next-line 
-    <iframe 
-      id="player" 
-      type="text/html" 
-      width="100%" 
-      height="640" 
-      src={`http://www.youtube.com/embed/${video.id}`} 
-      frameBorder="0" 
-      title="" />
+    <div>
+      {/* 비디오 상세 보기 */}
+      <section>
+        {/* eslint-disable-next-line */}
+        <iframe 
+          id="player" 
+          type="text/html" 
+          width="100%" 
+          height="640" 
+          src={`http://www.youtube.com/embed/${video.id}`} 
+          frameBorder="0" 
+          title="" />
+        <h1>{ title }</h1>
+        <ChanneInfo name={channelTitle} id={channelId} />
+        <pre>{description}</pre>
+      </section>
+      {/* 연관 비디오 목록 */}
+      <section>
+        <RelatedVideos id={video.id} />
+      </section>
+    </div> 
   );
 }
