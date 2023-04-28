3dbb793759a494c57955cbaa36b9cd3b9e8af1bd
hcs1105 <hcs1105@gmail.com>
2023-04-28 16:48:55 +0900

    마크업 수정

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -10,11 +10,9 @@ export default function VideoCard({ video, type }) {
   return (
     <li className={isList ? 'flex gap-1 m-2' : ''} onClick={() => {navigate(`/videos/watch/${video.id}`, {state: { video }});}}>
       <img src={thumbnails.medium.url} className={isList ? 'w-60 mr-2' : 'w-full'} alt="" />
-      <div> 
-        <h2 className="font-bold my-2 line-clamp-2">{title}</h2>
-        <p className="text-sm opacity-75">{channelTitle}</p>
-        <p className="text-sm opacity-75">{formatAgo(publishedAt, 'ko')}</p>
-      </div>
+      <h2 className="font-bold my-2 line-clamp-2">{title}</h2>
+      <p className="text-sm opacity-75">{channelTitle}</p>
+      <p className="text-sm opacity-75">{formatAgo(publishedAt, 'ko')}</p>
     </li>
   );
 }
